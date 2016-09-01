5f0f417403c66d1718fd8338f8a3652f00519aa0
Nedda Amini <nedda.amini@gmail.com>
2016-09-01 11:48:56 -0700

    Added .editorconfig file, fixed linting issues

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,51 +1,50 @@
-import { always } from 'ramda';
 import classNames from 'classnames';
-import React , { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import './Sidebar.css';
 
 export const NavItem = ({ label, grouped = false, expanded = false, linkTo }) => {
-    const classes = classNames({
-        'indent': grouped,
-        'collapse' : grouped && !expanded,
-        'toplevel': !grouped
-    });
-    return (
-        <div className="nav-item">
-            <button className={classes}>
-                {label}
-            </button>
-        </div>
-    );
+  const classes = classNames({
+    indent: grouped,
+    collapse: grouped && !expanded,
+    toplevel: !grouped,
+  });
+  return (
+    <div className="nav-item">
+      <button className={classes}>
+        {label}
+      </button>
+    </div>
+  );
 };
 
 
 export const NavGroup = ({ expanded, label, open, children, onNavGroupClick }) => {
-    const arrowClasses = "fa fa-fw fa-caret-" + (expanded ? 'down' : 'right');
-    let navOptions;
-    if(open){
-        navOptions = children;
-    }    
+  const arrowClasses = 'fa fa-fw fa-caret-' + (expanded ? 'down' : 'right');
+  let navOptions;
+  if (open) {
+    navOptions = children;
+  }
 
-    return (
-        <div className="nav-group">
-            <button onClick={onNavGroupClick}>
-                <i className={arrowClasses}></i>
-                {label}
-            </button>
-            {navOptions}
-        </div>
-    );
+  return (
+    <div className="nav-group">
+      <button onClick={onNavGroupClick}>
+        <i className={arrowClasses}></i>
+        {label}
+      </button>
+      {navOptions}
+    </div>
+  );
 };
 
 NavGroup.propTypes = {
-    open: PropTypes.bool
+  open: PropTypes.bool,
 };
 
-export const Sidebar = ( { children } ) => { 
-    return (
-        <div className="side-nav">
-            { children } 
-        </div>
-    );
-}
+export const Sidebar = ({ children }) => {
+  return (
+    <div className="side-nav">
+      { children }
+    </div>
+  );
+};
 
--- test/components/SidebarTest.js
+++ test/components/SidebarTest.js
@@ -4,40 +4,40 @@ import { equal, ok } from 'assert';
 import { Sidebar, NavGroup, NavItem } from '../../src/components/Sidebar';
 
 describe('Sidebar', () => {
-    it('should render Sidebar wrapper', () => {
-        const shallowWrapper = shallow(<Sidebar/>);
-        equal(shallowWrapper.hasClass('side-nav'), true);
+  it('should render Sidebar wrapper', () => {
+    const shallowWrapper = shallow(<Sidebar/>);
+    equal(shallowWrapper.hasClass('side-nav'), true);
+  });
+  describe('NavGroup', () => {
+    let shallowWrapper, hasBeenClicked, navItem;
+    describe('without dropdown', () => {
+      const clickDummy = () => { hasBeenClicked = true; };
+      beforeEach(() => {
+        hasBeenClicked = false;
+        shallowWrapper = shallow(<NavGroup label="something" onNavGroupClick={clickDummy}></NavGroup>);
+      });
+      it('should render NavGroup', () => {
+        equal(shallowWrapper.hasClass('nav-group'), true);
+      });
+      it('should handle an onClick function', () => {
+        shallowWrapper.find('button').simulate('click');
+        ok(hasBeenClicked);
+      });
     });
-    describe('NavGroup', () => {
-        let shallowWrapper, hasBeenClicked, navItem;
-        describe('without dropdown', () => {
-            const clickDummy = () => { hasBeenClicked = true; };
-            beforeEach(() => {
-                hasBeenClicked = false;
-                shallowWrapper = shallow(<NavGroup label="something" onNavGroupClick={clickDummy}></NavGroup>);
-            });
-            it('should render NavGroup', () => {
-                equal(shallowWrapper.hasClass('nav-group'), true);
-            });
-            it('should handle an onClick function', () => {
-                shallowWrapper.find('button').simulate('click');
-                ok(hasBeenClicked)
-            });
-        })
-        describe('with dropdown', () => {
-            const clickDummy = () => { hasBeenClicked = true; };
-            beforeEach(() => {
-                hasBeenClicked = false;
-                navItem = <NavItem label="something"/>
-                shallowWrapper = shallow(<NavGroup label="something" expanded={true}>{navItem}</NavGroup>);
-            });
-            it('should render internal NavItem', () => {
-                shallowWrapper.contains(navItem);
-            });
-        })
+    describe('with dropdown', () => {
+      beforeEach(() => {
+        hasBeenClicked = false;
+        navItem = <NavItem label="something"/>;
+        shallowWrapper = shallow(<NavGroup label="something" expanded={true}>{navItem}</NavGroup>);
+      });
+      it('should render internal NavItem', () => {
+        shallowWrapper.contains(navItem);
+      });
     });
-    it('should render NavItem independent of NavGroup', () => {
-        const shallowWrapper = shallow(<NavItem label="something" />);
-    });
+  });
+  it('should render NavItem independent of NavGroup', () => {
+    const shallowWrapper = shallow(<NavItem label="something" />);
+    equal(shallowWrapper.hasClass('nav-item'), true);
+  });
 });
 
