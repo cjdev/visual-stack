757bf6155bdc67cd6d5551a5d8132bd6b0199f32
Franklin Henderson <fhenderson@cj.com>
2016-12-06 11:33:57 -0800

    WIP top nav left

--- demos/src/App.js
+++ demos/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link as RRLink } from 'react-router';
 import './App.css';
 import { SideNav, Header, LinkGroup, Link } from '../../lib/components/SideNav';
-import { TopNav, LeftTopNav } from '../../lib/components/TopNav'
+import { TopNav } from '../../lib/components/TopNav'
 
 
 class App extends Component {
@@ -34,19 +34,21 @@ class App extends Component {
   }
 
   render() {
+    const LogoComponent = <span>[VV]</span>;
     return (
       <div className="app">
-        <TopNav>
-          <LeftTopNav>
-            <div className="app-logo">[VV]</div>
-            <div className="app-name">visual stack</div>
-          </LeftTopNav>
+        <TopNav
+          Logo={LogoComponent}
+          AppName="visual stack"
+        >
+
           <button onClick={this.onSideNavActiveClick}> Come back sidenav!  </button>
+
         </TopNav>
         <div className="app-container">
 
           <SideNav active={this.state.sidenavActive}>
-            <Header label="First Things"/>
+            <Header>First Things</Header>
             <Link><RRLink to="/components/button">Top Level Link</RRLink></Link>
             <LinkGroup expanded={this.state["Dashboards"]} label="Dashboards" onClick={this.onLinkGroupClick}>
               <Link><RRLink to="/">Program Overview</RRLink></Link>
@@ -55,7 +57,7 @@ class App extends Component {
               <Link><RRLink to="/">Eagle Eye</RRLink></Link>
               <Link><RRLink to="/">Dogs (woof!)</RRLink></Link>
             </LinkGroup>
-            <Header label="Other Things"/>
+            <Header>Other Things</Header>
             <Link><RRLink to="/about">About</RRLink></Link>
             <Link><a href="http://www.google.com" target="_blank">Google</a></Link>
           </SideNav>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './TopNav.css';
 
-export const LeftTopNav = ({ children }) => {
-  return (
-    <ul className="topnav left">
-      { children }
-    </ul>
-  );
-};
 
-export const TopNav = ({ children }) => {
+export const TopNav = ({ Logo, AppName, children }) => {
   return (
-    <ul className="topnav">
-      { children }
-    </ul>
+    <div className="topnav">
+      <div className="topnav-left">
+        <div className="topnav-left-logo">{ Logo }</div>
+        <div className="topnav-left-app-name">{ AppName }</div>
+      </div>
+        { children }
+    </div>
   );
 };
