1fad254aacb745c15a0ec8c357d33f66163395b7
Franklin Henderson <fhenderson@cj.com>
2017-03-29 13:52:10 -0700

    CJ logo, topnav / sidenav alignment, font, colors

--- packages/visual-stack-docs/src/containers/App/index.js
+++ packages/visual-stack-docs/src/containers/App/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import ReactSVG from 'react-svg';
 import { TopNav } from '@cjdev/visual-stack/lib/components/TopNav';
 import Layout from '@cjdev/visual-stack/lib/layouts/ApplicationLayout';
 import AppSideNav from './SideNav';
 import './styles.css';
+import logoSvg from '../../new-cj-logo-icon.svg';
+
+const logo = <ReactSVG
+  path={logoSvg}
+  className="cj-logo"
+/>;
 
 const AppTopNav = () =>
-  <TopNav appName="VISUAL stack" />;
+  <TopNav
+    appName="VISUAL stack"
+    logo={logo}
+  />;
 
 export default class App extends Component {
   render() {
