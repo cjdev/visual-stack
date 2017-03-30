b1a5f4a252a39e99a4a99297a9a0c55c73cfd815
Franklin Henderson <fhenderson@cj.com>
2017-03-30 12:27:50 -0700

    Move CJLogo to visual-stack from docs

--- packages/visual-stack-docs/src/containers/App/index.js
+++ packages/visual-stack-docs/src/containers/App/index.js
@@ -1,31 +1,17 @@
-import React, { Component } from 'react';
-import ReactSVG from 'react-svg';
+import React from 'react';
 import { TopNav } from '@cjdev/visual-stack/lib/components/TopNav';
+import CJLogo from '@cjdev/visual-stack/lib/components/CJLogo';
 import Layout from '@cjdev/visual-stack/lib/layouts/ApplicationLayout';
 import AppSideNav from './SideNav';
 import './styles.css';
-import logoSvg from '../../new-cj-logo-icon.svg';
-
-const logo = <ReactSVG
-  path={logoSvg}
-  className="cj-logo"
-/>;
 
 const AppTopNav = () =>
-  <TopNav
-    appName="VISUAL stack"
-    logo={logo}
-  />;
+  <TopNav appName="VISUAL stack" logo={<CJLogo />} />;
 
-export default class App extends Component {
-  render() {
-    return (
-      <Layout
-        topNav={<AppTopNav />}
-        sideNav={<AppSideNav/>} >
-        { this.props.children }
-      </Layout>
-    );
-  }
-}
+export default ({ children }) =>
+  <Layout
+    topNav={<AppTopNav />}
+    sideNav={<AppSideNav />} >
+    { children }
+  </Layout>;
 
