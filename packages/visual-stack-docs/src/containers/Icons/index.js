2fd08b55dfdcad4610bbfe8f7668e4cd8ea8bf16
Franklin Henderson <fhenderson@cj.com>
2017-09-22 09:41:47 -0700

    Initial Icons and Icon doc page

--- packages/visual-stack-docs/src/containers/Icons/index.js
+++ packages/visual-stack-docs/src/containers/Icons/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
 import PageContent from '@cjdev/visual-stack/lib/components/PageContent';
 import { PageHeader, PageTitle } from '@cjdev/visual-stack/lib/components/PageHeader';
+import { Panel, Body } from '@cjdev/visual-stack/lib/components/Panel';
+import './index.css';
+
+import * as IconSet from '@cjdev/visual-stack/lib/components/Icons/Nav';
+import { BarGraphIcon } from '@cjdev/visual-stack/lib/components/Icons/Nav';
+
+const IconDisplay = ({ icon, name }) => {
+  return (
+    <div className="icon-display">
+      <span className="icon-display-icon">{icon}</span>
+      <span className="icon-display-name">{name}</span>
+    </div>
+  );
+};
 
 const Icons = () =>
   <div>
@@ -9,8 +22,20 @@ const Icons = () =>
       <PageTitle>Icons</PageTitle>
     </PageHeader>
     <PageContent>
+      <div>
+        <Panel>
+          <Body>
+            <div className="icon-grid">
+              <IconDisplay icon={ <IconSet.AccountIcon/>} name="AccountIcon" />
+              <IconDisplay icon={ <IconSet.LogoutIcon/>} name="LogoutIcon" />
+              <IconDisplay icon={ <BarGraphIcon/>} name="BarGraphIcon" />
+              <IconDisplay icon={ <IconSet.ProductIcon/>} name="ProductIcon" />
+            </div>
+          </Body>
+        </Panel>
+      </div>
+
     </PageContent>
   </div>;
-
 
 export default Icons;
