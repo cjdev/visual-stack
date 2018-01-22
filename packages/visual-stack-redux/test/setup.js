9000e8e792b72de813ba333adda7a678807b39a8
zhuochengyu <zyu@cj.com>
2018-01-22 10:19:53 -0800

    remove id and added a comment to describe the closing behavior of menu dropdown

--- packages/visual-stack-redux/test/setup.js
+++ packages/visual-stack-redux/test/setup.js
@@ -4,6 +4,5 @@ chai.use(require('chai-enzyme')());
 
 global.expect = chai.expect;
 const mountPoint = document.createElement('div');
-mountPoint.id = 'mount';
 document.body.appendChild(mountPoint);
 global.mountPoint = mountPoint;
