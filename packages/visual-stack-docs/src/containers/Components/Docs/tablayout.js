8588178b38ff1e85c8a87a55837e68bd12fe0ee1
thekman <thekman@cj.com>
2018-03-07 16:09:35 -0800

    add styling to TabLayout components

--- packages/visual-stack-docs/src/containers/Components/Docs/tablayout.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/tablayout.js
@@ -7,19 +7,65 @@ export default () => (
   <Demo srcFile="/samples/src/containers/Components/Docs/tablayout.js">
     {
       snippets => {
+        /* s2:start */
+        const TabLabelContent = ({ ...props }) => <div style={{ padding: '15px 32px 11px', textAlign: 'center' }}>{props.children}</div>;
+        /* s2:end */
+
+        /* s3:start */
+        const TabContent = ({ ...props }) => <div style={{
+          padding: '20px',
+        }}>{props.children}</div>;
+        /* s3:end */
+
         return (
-          <Panel>
-            <Header>Simple Tab</Header>
-            <Body>
-              {/* s1:start */}
-              <TabLayout>
-                <Tab label={<div>Tab1</div>} content={<div>TibityTab1</div>} />
-                <Tab label={<div>Tab1</div>} content={<div>TibityTab1</div>} />
-              </TabLayout>
-              {/* s1:end */}
-              <Snippet tag="s1" src={snippets} />
-            </Body>
-          </Panel>
+          <div>
+            <Panel>
+              <Header>Base TabLayout</Header>
+              <Body>
+                <Snippet tag="s1" src={snippets} />
+                {/* s1:start */}
+                <TabLayout>
+                  <Tab label={<div>Disabled</div>} content={<div>Tab Content 0</div>} disabled={true}/>
+                  <Tab label={<div>Default</div>} content={<div>Tab Content 1</div>} />
+                  <Tab label={<div>Inactive</div>} content={<div>Tab Content 2</div>} />
+                </TabLayout>
+                {/* s1:end */}
+              </Body>
+            </Panel>
+            <Panel>
+              <Header>Tab can accept any component for label / content</Header>
+              <Body>
+                <div>Simple Label / Content Examples</div>
+                <Snippet tag="s2" src={snippets} />
+                <Snippet tag="s3" src={snippets} />
+                <Snippet tag="s4" src={snippets} />
+                {/* s4:start */}
+                <div style={{ border: '1px solid #e1e1e1' }}>
+                  <TabLayout>
+                    <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
+                    <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
+                    <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
+                  </TabLayout>
+                </div>
+                {/* s4:end */}
+              </Body>
+            </Panel>
+            <Panel>
+              <Header>TabLayout can take an additional onTabClick function prop</Header>
+              <Body>
+                <Snippet tag="s5" src={snippets} />
+                {/* s5:start */}
+                <div style={{ border: '1px solid #e1e1e1' }}>
+                  <TabLayout onTabClick={() => { /* function body */ }}>
+                    <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
+                    <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
+                    <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
+                  </TabLayout>
+                </div>
+                {/* s5:end */}
+              </Body>
+            </Panel>
+          </div>
         );
       }
     }
--- packages/visual-stack/src/components/TabLayout/index.js
+++ packages/visual-stack/src/components/TabLayout/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import R from 'ramda';
 import './TabLayout.css';
 
+const toArray = (maybeArray) => R.flatten([maybeArray]);
+
 export class TabLayout extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +15,7 @@ export class TabLayout extends React.Component {
   }
 
   componentDidMount() {
-    const children = this.props.children;
+    const children = toArray(this.props.children);
     this.setState({
       selectedIndex: R.findIndex(child => !child.props.disabled, children),
     });
@@ -23,40 +25,36 @@ export class TabLayout extends React.Component {
     this.setState({
       selectedIndex: index,
     });
-    const scrollingContainer = document.getElementsByClassName('application-layout-content')[0];
-
-    if (scrollingContainer) {
-      scrollingContainer.scrollTop = 0;
-    }
   }
 
   isSelected(index) {
-    // console.log("this.state.selectedIndex", this.state.selectedIndex);
     return index === this.state.selectedIndex;
   }
 
   render() {
-    const { floatingHeader, headerHeight } = this.props;
-    const children = this.props.children;
+    const { floatingHeader, headerHeight, headerWidth } = this.props;
+    const children = toArray(this.props.children);
     const tabs = R.filter(R.identity, children);
-    const labelMap = tabs.map((tab, index) => (
-      <TabLabel
-        {...tab.props}
-        key={index}
-        onTabClick={this.props.onTabClick}
-        onSelectClick={this.onSelectClick}
-        isSelected={this.isSelected}
-      />)
-    , tabs);
-    const contentMap = tabs.map((tab, index) => (
-      <div key={index} hidden={!this.isSelected(index)}>
+    const labelMap = tabs.map((tab, index) => {
+      return (
+        <TabLabel
+          {...tab.props}
+          key={index}
+          tabIndex={index}
+          onTabClick={this.props.onTabClick}
+          onSelectClick={this.onSelectClick}
+          isSelected={this.isSelected}>
+            {tab.props.label}
+        </TabLabel>);
+    });
+    const contentMap = tabs.map((tab, index) => {
+      return <div key={index} hidden={!this.isSelected(index)}>
         {tab.props.content}
-      </div>
-    ));
+      </div>;
+    });
     return (
       <div>
-        hull
-        <TabHeader floatingHeader={floatingHeader}>{labelMap}</TabHeader>
+        <TabHeader floatingHeader={floatingHeader} headerWidth={headerWidth}>{labelMap}</TabHeader>
         <TabBody floatingHeader={floatingHeader} headerHeight={headerHeight}>
           {contentMap}
         </TabBody>
@@ -65,81 +63,40 @@ export class TabLayout extends React.Component {
   }
 }
 
-export const Tab = ({ label, content }) => {};
+export const Tab = ({ label, content, disabled }) => {};
 
-const TabLabel = ({ label, disabled, index, onTabClick, onSelectClick, isSelected }) => (
-  <div className="cj-tab"
-    disabled={disabled ? disabled: false}
-    onClick={event => {
-      if (!disabled) {
-        onSelectClick(event, index);
-        if (onTabClick)
-          onTabClick();
-      }
-    }}
-  >
-    {label}
-  </div>
-);
+const TabLabel = ({ children, disabled, tabIndex, onTabClick, onSelectClick, isSelected }) => {
+  const labelStateClassName = disabled
+    ? 'cj-tab-label-disabled'
+    : isSelected(tabIndex) ? 'cj-tab-label-clicked' : 'cj-tab-label-inactive';
+  const labelClassName = `cj-tab-label ${labelStateClassName}`;
+  return (
+    <div className={labelClassName}
+      onClick={event => {
+        if (!disabled) {
+          onSelectClick(event, tabIndex);
+          if (onTabClick)
+            onTabClick();
+        }
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
-/* clicked={isSelected(index)} */
-// styled.div`
-//   border-right: 1px solid #e1e1e1;
-//   padding-bottom: 4px;
-//   background-color: #fff;
-//   transition: 0.2s color ease-in-out;
-//   user-select: none;
-//   display: flex;
-//   ${props =>
-//     props.disabled
-//       ? `color: #cecece;
-//         background-color: white`
-//       : props.clicked
-//         ? `color: #555;
-//           padding-bottom: 0px;
-//           & > * {
-//             border-bottom: 4px solid #048BC6;
-//           }`
-//         : `color: #888;
-//           &:hover {
-//           color: #048BC6;
-//           cursor: pointer;
-//       }`};
-// `;
-
-// non VS (CAS only)
-// export const TabContent = styled.div`
-//   padding-left: 20px;
-//   padding-top: 20px;
-// `;
-
-const TabHeader = ({ floatingHeader, children }) => {
-  return <div className="cj-tab" >
+const TabHeader = ({ children, floatingHeader, headerWidth }) => {
+  const floatingClassName = floatingHeader ? 'cj-tab-header-floating' : '';
+  const headerClassName = `cj-tab-header ${floatingClassName}`;
+  const headerCss = floatingHeader ? { width: `${headerWidth}` } : {};
+  return <div className={headerClassName} style={headerCss}>
     {children}
   </div>;
-}
-;
+};
 
-
-// ;;
-// styled.div`
-//   ${props =>
-//     props.floatingHeader ? 'position: fixed;' : ''} box-shadow: 0 0px 32px 0 rgba(136,136,136,0.25);
-//   border-bottom: 1px solid #e1e1e1;
-//   display: flex;
-//   width: 100%;
-//   background-color: white;
-// `;
-
-// non VS (CAS only)
-// export const TabLabelContent = styled.div`
-//   padding: 15px 32px 11px;
-//   text-align: center;
-// `;
-
-
-
-const TabBody = ({ children }) => <div className="cj-tab">{children}</div>;
-// styled.div`
-//   ${props => (props.floatingHeader ? `padding-top: ${props.headerHeight};` : '')};
-// `;
+const TabBody = ({ children, floatingHeader, headerHeight }) => {
+  const headerHeightCss = floatingHeader ? { 'padding-top': `${headerHeight}` } : {};
+  return (<div style={ headerHeightCss }>
+    {children}
+  </div>);
+};
