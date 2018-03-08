e83b79bb4504e242f766ad8d11f4c4198325a1a7
jeslin <jeslin@cj.com>
2018-03-08 13:54:48 -0800

    add floating header to docs

--- packages/visual-stack-docs/src/containers/Components/Docs/tablayout.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/tablayout.js
@@ -17,57 +17,82 @@ export default () => (
         }}>{props.children}</div>;
         /* s3:end */
 
+        const FullPageWrapper = ({ ...props }) => <div style={{ margin: '-15px -1em -1em' }}>{props.children}</div>;
+
         return (
-          <div>
-            <Panel>
-              <Header>Base TabLayout</Header>
-              <Body>
-                <Snippet tag="s1" src={snippets} />
-                {/* s1:start */}
-                <TabLayout>
-                  <Tab label={<div>Disabled</div>} content={<div>Tab Content 0</div>} disabled={true}/>
-                  <Tab label={<div>Default</div>} content={<div>Tab Content 1</div>} />
-                  <Tab label={<div>Inactive</div>} content={<div>Tab Content 2</div>} />
-                </TabLayout>
-                {/* s1:end */}
-              </Body>
-            </Panel>
-            <Panel>
-              <Header>Tab can accept any component for label / content</Header>
-              <Body>
-                <div>Simple Label / Content Examples</div>
-                <Snippet tag="s2" src={snippets} />
-                <Snippet tag="s3" src={snippets} />
-                <Snippet tag="s4" src={snippets} />
-                {/* s4:start */}
-                <div style={{ border: '1px solid #e1e1e1' }}>
-                  <TabLayout>
-                    <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
-                    <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
-                    <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
-                  </TabLayout>
-                </div>
-                {/* s4:end */}
-              </Body>
-            </Panel>
-            <Panel>
-              <Header>
-                <div>TabLayout can take additional props: onTabClick, themeColor</div>
-                </Header>
-              <Body>
-                <Snippet tag="s5" src={snippets} />
-                {/* s5:start */}
-                <div style={{ border: '1px solid #e1e1e1' }}>
-                  <TabLayout onTabClick={() => { /* function body */ }} themeColor={'#048BC6'}>
-                    <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
-                    <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
-                    <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
-                  </TabLayout>
-                </div>
-                {/* s5:end */}
-              </Body>
-            </Panel>
-          </div>
+          <FullPageWrapper>
+            {/* s6:start */}
+            <TabLayout floatingHeader={true} headerHeight={'49px'} headerWidth={'100%'}>
+            {/* s6:end */}
+              <Tab
+                label={<TabLabelContent>Tab 1</TabLabelContent>}
+                content={
+                  <TabContent >
+                    <Panel>
+                      <Header>Base TabLayout</Header>
+                      <Body>
+                        <Snippet tag="s1" src={snippets} />
+                        {/* s1:start */}
+                        <TabLayout>
+                          <Tab label={<div>Disabled</div>} content={<div>Tab Content 0</div>} disabled={true}/>
+                          <Tab label={<div>Default</div>} content={<div>Tab Content 1</div>} />
+                          <Tab label={<div>Inactive</div>} content={<div>Tab Content 2</div>} />
+                        </TabLayout>
+                        {/* s1:end */}
+                      </Body>
+                    </Panel>
+                    <Panel>
+                      <Header>Tab can accept any component for label / content</Header>
+                      <Body>
+                        <div>Simple Label / Content Examples</div>
+                        <Snippet tag="s2" src={snippets} />
+                        <Snippet tag="s3" src={snippets} />
+                        <Snippet tag="s4" src={snippets} />
+                        {/* s4:start */}
+                        <div style={{ border: '1px solid #e1e1e1' }}>
+                          <TabLayout>
+                            <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
+                            <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
+                            <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
+                          </TabLayout>
+                        </div>
+                        {/* s4:end */}
+                      </Body>
+                    </Panel>
+                    <Panel>
+                      <Header>
+                        <div>TabLayout can take additional props: onTabClick, themeColor</div>
+                        </Header>
+                      <Body>
+                        <Snippet tag="s5" src={snippets} />
+                        {/* s5:start */}
+                        <div style={{ border: '1px solid #e1e1e1' }}>
+                          <TabLayout onTabClick={() => { /* function body */ }} themeColor={'#048BC6'}>
+                            <Tab label={<TabLabelContent>Tab 1</TabLabelContent>} content={<TabContent>Tab Content 1</TabContent>} />
+                            <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
+                            <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
+                          </TabLayout>
+                        </div>
+                        {/* s5:end */}
+                      </Body>
+                    </Panel>
+                    <Panel>
+                      <Header>
+                        <div>Floating Header (as seen at the top of this page)</div>
+                      </Header>
+                      <Body>
+                        <Snippet tag="s6" src={snippets} />
+                        <div>
+                          The headerHeight prop does not set the height of the header. It specifies the vertical offset for tab body content.
+                        </div>
+                      </Body>
+                    </Panel>
+                  </TabContent>
+                } />
+              <Tab label={<TabLabelContent>Tab 2</TabLabelContent>} content={<TabContent>Tab Content 2</TabContent>} />
+              <Tab label={<TabLabelContent>Tab 3</TabLabelContent>} content={<TabContent>Tab Content 3</TabContent>} />
+            </TabLayout>
+          </FullPageWrapper>
         );
       }
     }
